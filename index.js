0b243a2996b7819167276aaa0330f4c82a7c157d
harry-lkl <harryk742121@gmail.com>
2023-02-20 16:53:01 +0000

    initiate html boiler plate and basic funnction planning

---
+++ index.js
@@ -0,0 +1,23 @@
+/* basic functions:
+    detect click and keypress
+    clear all entry (C)
+    clear entry (CE)
+    backspace
+    calculation:
+        add
+        subtract
+        multiply
+        divide
+        percent
+        1/x
+        x^2
+        sqrt(x)
+        decimal
+    memory (maybe):
+        memory clear (MC)
+        memory recall (MR)
+        memory+ (M+)
+        memory- (M-)
+        memory store (MS)
+
+*/
