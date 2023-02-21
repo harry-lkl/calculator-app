f2ea4c623503d84b92711fb624de9f62833d14f2
harry-lkl <harryk742121@gmail.com>
2023-02-21 01:45:41 +0000

    modify the operation functions to compute from pre-defined variables; write operate function to execute the operation functions depending on the pre-defined variable

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
-function add(a, b) {
-    return a + b;
+let operator = '';
+let a = 0;
+let b = 0;
+let result = 0;
+
+function add() {
+    return result = a + b;
 }
 
-function subtract(a, b) {
-    return a - b;
+function subtract() {
+    return result = a - b;
 }
 
-function multiply(a, b) {
-    return a * b;
+function multiply() {
+    return result = a * b;
 }
 
-function divide(a, b) {
-    return a / b;
+function divide() {
+    if (b === 0) return;
+    return result = a / b;
 }
+
+function operate() {
+    switch(operator) {
+        case 'add':
+            add();
+            break;
+        case 'subtract':
+            subtract();
+            break;
+        case 'multiply':
+            multiply();
+            break;
+        case 'divide':
+            divide();
+    }
+    }
